832c0f83e2eb6249c141555dbbca5dbbc77fc152
Bobbi Colhour <bobbicolhour@hotmail.com>
2022-10-22 23:20:17 -0500

    adding to upper file

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -17,9 +17,9 @@
 import csv
 import os
 # Assign a variable for the file to load and the path.
-file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_load = os.path.join("election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("../analysis", "election_analysis.txt")
 
 # 1. Initialize a total vote counter. 
 total_votes=0
